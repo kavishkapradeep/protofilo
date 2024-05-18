6212fb0825c9f883e304726cb35ee66f893679f9
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-05-18 14:06:57 +0530

    connect linkidin,github  and facebook

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 function facebook() {
     window.location.href="https://www.facebook.com/profile.php?id=100072903760866&mibextid=LQQJ4d";
 }
+
+function footerFacebook() {
+    
+ 
+    window.location.href="https://www.facebook.com/profile.php?id=100072903760866&mibextid=LQQJ4d";
+    }
